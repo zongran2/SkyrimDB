97fe87d90f205c9f7a4aeb728f4bde9aae802abb
Serge Ivanov <lutvuk@gmail.com>
2012-07-25 17:01:31 +0400

    Fixed fragments preview in designer

--- src/com/sivanov/skyrimdb/DataDisplayFragment.java
+++ src/com/sivanov/skyrimdb/DataDisplayFragment.java
@@ -1,7 +1,15 @@
 package com.sivanov.skyrimdb;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DataDisplayFragment extends ListFragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.data_dispay_fragment, container, false);
+        return view;
+    }
 }
