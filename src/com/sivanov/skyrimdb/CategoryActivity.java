4e3b14ef6a836b75931c50eb5ac0a047d87ae703
Serge Ivanov <lutvuk@gmail.com>
2012-07-25 17:01:31 +0400

    Fixed fragments preview in designer

--- src/com/sivanov/skyrimdb/CategoryActivity.java
+++ src/com/sivanov/skyrimdb/CategoryActivity.java
@@ -1,6 +1,7 @@
 package com.sivanov.skyrimdb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -11,16 +12,16 @@ public class CategoryActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
-        
+
         View show_perks_btn = findViewById(R.id.show_perks_btn);
         show_perks_btn.setOnClickListener(new View.OnClickListener() {
-	    
-	    @Override
-	    public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-	    }
-	});
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), DataDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- src/com/sivanov/skyrimdb/SearchDataFragment.java
+++ src/com/sivanov/skyrimdb/SearchDataFragment.java
@@ -1,7 +1,15 @@
 package com.sivanov.skyrimdb;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SearchDataFragment extends Fragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_fragment, container, false);
+        return view;
+    }
 }
